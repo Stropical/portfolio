d8c07f61f851a2716e59cc1aef35c985982b80d8
Ethan Marreel <ethan@marreel.com>
2025-10-06 11:35:59 -0700

    Deploy to GitHub Pages: 2025-10-06 18:35:59

--- lib/paths.ts
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-const basePath = isProd ? '/portfolio' : ''
-
-export const getAssetPath = (path: string) => {
-  if (path.startsWith('http')) return path // External URLs
-  return `${basePath}${path}`
-}
-
-export const getBasePath = () => basePath
